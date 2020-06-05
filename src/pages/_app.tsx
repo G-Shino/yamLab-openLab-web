3dadec77aab5dff71153bb91f60a0cc1031f9d8a
G-shino <shinogu2929@gmail.com>
2020-06-05 21:56:59 +0900

    add head at app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { NextComponentType } from "next";
 import { AppContext, AppInitialProps, AppProps } from "next/app";
 import { Global } from "@emotion/core";
 import globalCSS from "./../styles/global";
+import Head from "next/head";
 
 const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
@@ -10,6 +11,15 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
 }) => {
   return (
     <>
+      <Head>
+        <title>山中研究室OpenLab</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Global styles={globalCSS} />
       <Component {...pageProps} />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Head from "next/head";
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Head>
-        <title>Hello</title>
-      </Head>
       <h1>Hello Next</h1>
     </div>
   );
