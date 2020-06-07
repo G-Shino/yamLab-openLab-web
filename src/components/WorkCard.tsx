b5393f8fc9539420f822d08259e808a89e52ea44
G-shino <shinogu2929@gmail.com>
2020-06-07 17:25:49 +0900

    add works

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,40 +1,107 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Color } from "../constants/Color";
+import Router from "next/router";
 
 interface WorkCardProps {
   src: string;
   titleJP: string;
   titleEN: string;
+  link: string;
 }
 
-const WorkCard: React.FC<WorkCardProps> = ({ src, titleJP, titleEN }) => {
+const WorkCard: React.FC<WorkCardProps> = ({ src, titleJP, titleEN, link }) => {
   return (
-    <CardDiv>
+    <CardDiv
+      onClick={() => {
+        Router.push(link);
+      }}
+    >
       <ImageDiv>
         <Image src={src} />
       </ImageDiv>
       <TextDiv>
-        <TitleJP>{titleJP}</TitleJP>
-        <TitleEN>{titleEN}</TitleEN>
+        {titleJP === "" ? (
+          <></>
+        ) : (
+          <TitleJPDiv>
+            <TitleJP>{titleJP}</TitleJP>
+          </TitleJPDiv>
+        )}
+
+        <TitleENDiv>
+          <TitleEN>{titleEN}</TitleEN>
+        </TitleENDiv>
       </TextDiv>
     </CardDiv>
   );
 };
 
 const CardDiv = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 320px;
+  height: auto;
+  transition: transform 0.1s linear;
+  margin: 0 40px 80px;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 const ImageDiv = styled.div`
   width: 100%;
+  height: 320px;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
-const TextDiv = styled.div``;
+const TextDiv = styled.div`
+  height: 96px;
+  padding: 8px;
+  background-color: ${Color.CAPTION_COLOR};
+  text-align: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
 
-const TitleJP = styled.p``;
-const TitleEN = styled.p``;
+const TitleJPDiv = styled.div`
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+`;
+
+const TitleJP = styled.p`
+  font-size: 3.5rem;
+  line-height: 4.2rem;
+  color: ${Color.CAPTION_FONT_COLOR};
+  white-space: nowrap;
+`;
+
+const TitleENDiv = styled.div`
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+`;
+
+const TitleEN = styled.p`
+  width: 100%;
+  font-size: 3.5rem;
+  line-height: 4.2rem;
+  color: ${Color.CAPTION_FONT_COLOR};
+  white-space: nowrap;
+`;
 
 export default WorkCard;
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -11,71 +11,97 @@ const Heejun = require("../images/works/Heejun.jpg");
 const Hazuki = require("../images/works/Hazuki.jpg");
 const Fumin = require("../images/works/Fu-min.jpg");
 
-const LIST_IMG_OBJ = [
+interface IMG_OBJ {
+  src: string;
+  author: string;
+  titleJP: string;
+  titleEN: string;
+  link: string;
+}
+const LIST_IMG_OBJ: IMG_OBJ[] = [
   {
     src: Uena,
     author: "Uena",
     titleJP: "ゆらゆら",
+    titleEN: "solid swing",
     link: "/works/uena",
   },
   {
     src: Takuro,
     author: "Takuro",
     titleJP: "Rami S",
+    titleEN: "Rami S",
     link: "/works/takuro",
   },
   {
     src: Shinogu,
     author: "Shinogu",
     titleJP: "流点",
+    titleEN: "Ruten",
     link: "/works/shinogu",
   },
   {
     src: Oto,
     author: "Oto",
     titleJP: "道具の暇",
+    titleEN: "Tool's Leisure Time",
     link: "/works/oto",
   },
   {
     src: Oga,
     author: "Oga",
     titleJP: "Scalable hand",
+    titleEN: "Scalable hand",
     link: "/works/oga",
   },
   {
     src: Kana,
     author: "Kana",
     titleJP: "OTT: OTTOTTO",
+    titleEN: "Oops Robot",
     link: "/works/kana",
   },
   {
     src: Heejun,
     author: "Heejun",
     titleJP: "チタンの家具",
+    titleEN: "Titanium Furniture",
     link: "/works/heejun",
   },
   {
     src: Hazuki,
     author: "Hazuki",
-    titleJP: "長いタイトル",
+    titleJP: "",
+    titleEN:
+      "Reduce Unconscious Gender Bias through Workshop with Co-Speculative Design",
     link: "/works/hazuki",
   },
   {
     src: Fumin,
     author: "Fumin",
-    titleJP: "Chin & Shoulder Rest",
+    titleJP: "ヴァイオリイン用顎・肩当て",
+    titleEN: "Chin & Shoulder Rest",
     link: "/works/fumin",
   },
 ];
 
-const PreviousDialog: React.FC = () => {
+const Works: React.FC = () => {
   return (
     <WrapperCSS>
-      <WorkCard
-        src={LIST_IMG_OBJ[0].src}
-        titleJP={LIST_IMG_OBJ[0].titleJP}
-        titleEN={"DUMMY"}
-      />
+      <SectionTitleDiv>
+        <SectionTitle>- Works -</SectionTitle>
+      </SectionTitleDiv>
+      <SectionWorksDiv>
+        {LIST_IMG_OBJ.map((imgObj) => (
+          <WorkCard
+            key={imgObj.author}
+            src={imgObj.src}
+            titleJP={imgObj.titleJP}
+            titleEN={imgObj.titleEN}
+            link={imgObj.link}
+          />
+        ))}
+      </SectionWorksDiv>
     </WrapperCSS>
   );
 };
@@ -83,7 +109,25 @@ const PreviousDialog: React.FC = () => {
 //Styles---------------------------------------------------------------
 const WrapperCSS = styled.div`
   width: 90%;
-  min-width: 1000px;
+  max-width: 1200px;
 `;
 
-export default PreviousDialog;
+const SectionTitleDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 104px;
+`;
+
+const SectionTitle = styled.p`
+  font-size: 4rem;
+`;
+
+const SectionWorksDiv = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export default Works;
