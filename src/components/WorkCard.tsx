a1c50d65383f900f53bb0858497608eb685c6a42
G-shino <shinogu2929@gmail.com>
2020-06-09 01:45:38 +0900

    del card text

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Color } from "../constants/Color";
+// import { Color } from "../constants/Color";
 import Router from "next/router";
 
 interface WorkCardProps {
@@ -10,7 +10,7 @@ interface WorkCardProps {
   link: string;
 }
 
-const WorkCard: React.FC<WorkCardProps> = ({ src, titleJP, titleEN, link }) => {
+const WorkCard: React.FC<WorkCardProps> = ({ src, link }) => {
   return (
     <CardDiv
       onClick={() => {
@@ -20,19 +20,6 @@ const WorkCard: React.FC<WorkCardProps> = ({ src, titleJP, titleEN, link }) => {
       <ImageDiv>
         <Image src={src} />
       </ImageDiv>
-      <TextDiv>
-        {titleJP === "" ? (
-          <></>
-        ) : (
-          <TitleJPDiv>
-            <TitleJP>{titleJP}</TitleJP>
-          </TitleJPDiv>
-        )}
-
-        <TitleENDiv>
-          <TitleEN>{titleEN}</TitleEN>
-        </TitleENDiv>
-      </TextDiv>
     </CardDiv>
   );
 };
@@ -91,55 +78,6 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-`;
-
-const TextDiv = styled.div`
-  height: 96px;
-  padding: 8px;
-  background-color: ${Color.CAPTION_COLOR};
-  text-align: center;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-`;
-
-const TitleJPDiv = styled.div`
-  width: 100%;
-  overflow-x: auto;
-  overflow-y: hidden;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  z-index: 3;
-`;
-
-const TitleJP = styled.p`
-  width: 100%;
-  font-size: 3.5rem;
-  line-height: 4.2rem;
-  color: ${Color.CAPTION_FONT_COLOR};
-  white-space: nowrap;
-`;
-
-const TitleENDiv = styled.div`
-  width: 100%;
-  overflow-x: auto;
-  overflow-y: hidden;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  z-index: 3;
-`;
-
-const TitleEN = styled.p`
-  width: 100%;
-  font-size: 3.5rem;
-  line-height: 4.2rem;
-  color: ${Color.CAPTION_FONT_COLOR};
-  white-space: nowrap;
 `;
 
 export default WorkCard;
