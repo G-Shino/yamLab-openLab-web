9017dc524976ed23d16ded0e27ce3de72408134c
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-06-13 21:08:54 +0900

    .

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -7,18 +7,17 @@ const Credits = () => {
       <CreditWrapper>
         <CreditLeftCSS>
           {" "}
-          ディレクション： 山中俊治 <br></br>企画： 杉本拓郎　イ
-          ヒジュン　山本凌　大西彬介<br></br> 　 　　今村知美　李 馨 <br></br>
-          制作： イヒジュン　上岡直樹　小笠原佑樹　金山正貴<br></br> 　
-          　　川又音　杉本拓郎　髙田ふみ　三好葉月<br></br> 　
-          　　宗像佑弥　山本 凌
+          ディレクション： 山中俊治 <br></br>企画： 杉本拓郎　イ ヒジュン　山本
+          凌　大西彬介<br></br> 　 　　今村知美　李 馨 <br></br>
+          制作： イヒジュン　上岡直樹　小笠原佑樹　金山正貴<br></br> 　 　　川又
+          音　杉本拓郎　髙田ふみ　三好葉月<br></br> 　 　　宗像佑弥　山本 凌
         </CreditLeftCSS>
         <CreditRightCSS>
           <br></br>
           スタッフ： 東京大学 山中研究室一同<br></br>
           主催： 東京大学 山中研究室<br></br>
           グラフィック： 李 馨　髙田ふみ　杉本拓郎<br></br>
-          WEB : イ ヒジュン 山本 凌<br></br>
+          WEB : イ ヒジュン　山本 凌<br></br>
           翻訳：イ ヒジュン
         </CreditRightCSS>
       </CreditWrapper>
