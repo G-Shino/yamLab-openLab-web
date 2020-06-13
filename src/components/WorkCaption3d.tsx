a1c7aa6b7e2a6c51884b6a4404f777d89a2ff22f
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-06-13 17:11:36 +0900

    一時保存用

---
+++ src/components/WorkCaption3d.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { Color } from "../constants/Color";
+
+interface Props {
+  Caption3dImage?: React.ReactNode;
+  captionTitleJP: string;
+  captionTitleEN: string;
+  captionMessageJP: string;
+  captionMessageEN: string;
+}
+
+const WorkCaption3d: React.FC<Props> = ({
+  Caption3dImage,
+  captionTitleJP,
+  captionTitleEN,
+  captionMessageJP,
+  captionMessageEN,
+}) => {
+  return (
+    <CaptionWrapperDiv>
+      {Caption3dImage}
+      <CaptionMessageDiv>
+        <CaptionTitleJP>{captionTitleJP}</CaptionTitleJP>
+        <CaptionTitleEN>- {captionTitleEN} -</CaptionTitleEN>
+        <CaptionMessageJP>{captionMessageJP}</CaptionMessageJP>
+        <CaptionMessageEN>{captionMessageEN}</CaptionMessageEN>
+      </CaptionMessageDiv>
+    </CaptionWrapperDiv>
+  );
+};
+
+export default WorkCaption3d;
+
+const CaptionWrapperDiv = styled.div`
+  width: 100%;
+
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-end;
+
+  @media (min-width: 1000px) {
+    max-width: 50%;
+  }
+`;
+
+const Caption3dImg = styled.div`
+  width: 100%;
+`;
+
+const CaptionMessageDiv = styled.div`
+  width: 100%;
+  color: ${Color.CAPTION_FONT_COLOR};
+  background-color: ${Color.CAPTION_COLOR};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CaptionTitleJP = styled.h3`
+  width: 90%;
+  font-weight: bold;
+  margin-top: 3.2rem;
+`;
+const CaptionTitleEN = styled.h3`
+  width: 90%;
+  font-weight: bold;
+  margin-bottom: 1.6rem;
+`;
+const CaptionMessageJP = styled.p`
+  width: 90%;
+  margin-bottom: 1.6rem;
+`;
+const CaptionMessageEN = styled.p`
+  width: 90%;
+  margin-bottom: 3.2rem;
+`;
---
+++ src/components/orbit.tsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { ReactThreeFiber, extend, useThree, useFrame } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls });
+
+// インターフェイスIntrinsicElementsにorbitControls の定義を追加
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Node<OrbitControls, typeof OrbitControls>;
+    }
+  }
+}
+
+type ControlProps = {
+  isControl: boolean;
+};
+
+const Controls: React.FC<ControlProps> = (props) => {
+  const controlsRef = useRef<OrbitControls>();
+  const { camera, gl } = useThree();
+
+  useFrame(() => {
+    controlsRef.current?.update();
+  });
+
+  return (
+    <orbitControls
+      ref={controlsRef}
+      args={[camera, gl.domElement]}
+      enabled={props.isControl}
+      enableZoom={true}
+      zoomSpeed={1.0}
+      enableRotate={true}
+      rotateSpeed={1.0}
+      enablePan={true}
+      panSpeed={2.0}
+      minDistance={0}
+      maxDistance={Infinity}
+      minPolarAngle={0}
+      maxPolarAngle={Math.PI}
+    />
+  );
+};
+export default Controls;
