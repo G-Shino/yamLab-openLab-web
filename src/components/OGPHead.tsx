2f95ca8f0741fbf16e11891e309d8ea629ebe539
G-shino <shinogu2929@gmail.com>
2020-06-16 21:03:18 +0900

    fix card name

--- src/components/OGPHead.tsx
+++ src/components/OGPHead.tsx
@@ -25,7 +25,7 @@ const OGPHead = ({
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content="山中研究室OpenLab" />
       <meta property="og:image" content={url + image} />
-      <meta name="twitter:card" content="Summary Card" />
+      <meta name="twitter:card" content="summary" />
     </Head>
   );
 };
