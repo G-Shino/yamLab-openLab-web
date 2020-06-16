7086a331d0ffbdc250aa9221bbee46fb510e7440
G-shino <shinogu2929@gmail.com>
2020-06-16 19:11:16 +0900

    add share button

--- src/components/FootSpace.tsx
+++ src/components/FootSpace.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import styled from "@emotion/styled";
 import FooterCarousel from "./FooterCarousel";
 import { Color } from "../constants/Color";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LineShareButton,
+  LineIcon,
+} from "react-share";
 
 interface Props {
   src: string;
@@ -13,10 +21,32 @@ interface Props {
 }
 
 const FootSpace: React.FC<Props> = ({ src, author }) => {
+  // React.useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "https://platform.twitter.com/widgets.js";
+  //   document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+  // }, []);
   return (
     <Footer>
       <FootContentWrapperDiv>
         <HandwritingImg src={src} />
+        <StyledTwitterButton className="twitter-embed">
+          <TwitterShareButton
+            url="https://yam-lab-open-lab-web-sable.now.sh/"
+            hashtags={["#DiaLog"]}
+          >
+            <TwitterIcon size={64} round />
+          </TwitterShareButton>
+          <FacebookShareButton
+            url="https://yam-lab-open-lab-web-sable.now.sh/"
+            hashtag="#DiaLog"
+          >
+            <FacebookIcon size={64} round />
+          </FacebookShareButton>
+          <LineShareButton url="https://yam-lab-open-lab-web-sable.now.sh/">
+            <LineIcon size={64} round />
+          </LineShareButton>
+        </StyledTwitterButton>
         <CarouselDiv>
           <FooterCarousel exclude={author} />
         </CarouselDiv>
@@ -37,7 +67,7 @@ const FootContentWrapperDiv = styled.div`
   width: 85%;
   max-width: 1000px;
   height: auto;
-  margin: 0 auto 32px;
+  margin: 0 auto 48px;
   @media (min-width: 1000px) {
     width: 42.5%;
   }
@@ -46,8 +76,7 @@ const FootContentWrapperDiv = styled.div`
 const HandwritingImg = styled.img`
   width: 100%;
   max-width: 500px;
-  margin: 0 auto;
-  filter: invert();
+  margin: 48px auto;
   display: block;
 `;
 
@@ -56,4 +85,12 @@ const CarouselDiv = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+const StyledTwitterButton = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 48px;
 `;
