924703c5d75d0af938453ea9b9b4cab6f29c8f94
lhjrobo <63150314+lhjrobo@users.noreply.github.com>
2020-06-17 20:46:22 +0900

    bugfix

--- src/pages/works/heejun.tsx
+++ src/pages/works/heejun.tsx
@@ -10,7 +10,7 @@ import Caption1Image from "../../images/works/Uena/caption1.png";
 //import Caption2Image from "../../images/works/Uena/caption2.png";
 //import Caption3Image from "../../images/works/Uena/caption3.png";
 import ChairScene from "../../components/heejunscene";
-import HandWritingImage from "../../images/works/heejun/Heejun-handwriting.png";
+import HandWritingImage from "../../images/works/heejun/heejun-handwriting.png";
 import {
   WorksBaseInfoWithPrevAndNextLink,
   getWorksInfoByAuthor,
